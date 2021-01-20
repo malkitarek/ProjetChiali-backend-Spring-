de4ac63a0fc09a2a8df2d7243f4b747c1c06cad7
malkitarek <malkitarek96@gmail.com>
2021-01-20 15:11:14 +0100

    first commit

--- src/main/java/org/sid/BackendApplication.java
+++ src/main/java/org/sid/BackendApplication.java
@@ -34,12 +34,8 @@ public class BackendApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Compte cc=new Compte(null,"admin2",bCryptPasswordEncoder.encode("1234"),
-                "","0557882216","ADMIN");
-        compteRepository.save(cc);
-        Admin admin=new Admin(null,"Malkii","Tarekk",cc);
-        adminRepository.save(admin);
-       /*  Compte cc=new Compte(null,"admin",bCryptPasswordEncoder.encode("1234"),
+
+         Compte cc=new Compte(null,"admin",bCryptPasswordEncoder.encode("1234"),
                 "","0557882216","ADMIN");
         compteRepository.save(cc);
         Admin admin=new Admin(null,"Malki","Tarek",cc);
@@ -94,7 +90,7 @@ public class BackendApplication implements CommandLineRunner {
         villeRsp.save(new Ville(null,45L,"Naâma",null));
         villeRsp.save(new Ville(null,46L,"Ain Témouchent",null));
         villeRsp.save(new Ville(null,47L,"Ghardaia",null));
-        villeRsp.save(new Ville(null,48L,"Relizane",null));*/
+        villeRsp.save(new Ville(null,48L,"Relizane",null));
 
     }
 }
